35b28c214c57e62b31d3b48bfac92bf91ca54633
Zahid Hasan <mrzahidxy.com>
2024-12-10 23:24:53 +0600

    modify: add product ui

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -1,25 +1,42 @@
 "use client";
 
-import { TProductBody } from "@/types/product";
 import { useMutation } from "@tanstack/react-query";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { z } from "zod";
 
 type Inputs = {
   title: string;
   desc: string;
   price: number;
   catSlug: string;
+  isFeatured: boolean;
 };
 
 type Option = {
   title: string;
   additionalPrice: number;
 };
+
+const productSchema = z.object({
+  title: z.string().min(1, "Title is required"),
+  desc: z.string().min(1, "Description is required"),
+  price: z.number().positive("Price must be greater than 0"),
+  catSlug: z.string().min(1, "Category is required"),
+  options: z.array(
+    z.object({
+      title: z.string().min(1, "Option title is required"),
+      additionalPrice: z.number().min(0, "Additional price must be at least 0"),
+    })
+  ),
+  file: z.any().refine((file) => file && typeof file === "object", {
+    message: "File must be uploaded",
+  }),
+});
 
 const AddPage = () => {
   const { data: session, status } = useSession();
@@ -31,8 +48,8 @@ const AddPage = () => {
     desc: "",
     price: 0,
     catSlug: "pizzas",
+    isFeatured: false,
   });
-
   const [option, setOption] = useState<Option>({
     title: "",
     additionalPrice: 0,
@@ -42,19 +59,23 @@ const AddPage = () => {
 
   const handleChange = (
     e: React.ChangeEvent<
-      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement 
     >
   ) => {
-    const { name, value } = e.target;
-    const parsedValue = name === "price" ? parseInt(value) : value;
+    const { name, type,value } = e.target;
+    const checked = (e.target as HTMLInputElement).checked;
+
+    const isPrice = name === "price";
+    const parsedPrice = isPrice ? parseInt(value) : value;
+    const newValue = type === "checkbox" ? checked : parsedPrice;
 
     setInputs((prev) => {
-      return { ...prev, [e.target.name]: parsedValue };
+      return { ...prev, [e.target.name]: newValue };
     });
   };
 
   const handleChangeOptions = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    e: React.ChangeEvent<HTMLInputElement>
   ) => {
     const { name, value } = e.target;
     const parsedValue = name === "additionalPrice" ? parseInt(value) : value;
@@ -64,40 +85,25 @@ const AddPage = () => {
     });
   };
 
+  const handleAddOption = () => {
+    setOptions((prev) => [...prev, option]);
+    setOption({ title: "", additionalPrice: 0 });
+  };
+
   const handleChangeImg = (e: React.ChangeEvent<HTMLInputElement>) => {
     const target = e.target as HTMLInputElement;
     const item = (target.files as FileList)[0];
     setFile(item);
   };
 
-  const upload = async () => {
-    const formData = new FormData();
-    formData.append("file", file!);
-    formData.append("upload_preset", "resturant-app");
-
-    const res = await fetch(
-      "https://api.cloudinary.com/v1_1/mrzahidxy/image/upload",
-      {
-        method: "POST",
-        body: formData,
-      }
-    );
-
-    const resData = await res.json();
-
-    return resData.url;
-  };
-
+  // Add product mutation & submission
   const mutation = useMutation({
-    mutationFn: async (body: TProductBody) => {
+    mutationFn: async (formData: FormData) => {
       const response = await fetch(
         `${process.env.NEXT_PUBLIC_API_URL}/products`,
         {
           method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(body),
+          body: formData,
         }
       );
 
@@ -111,62 +117,40 @@ const AddPage = () => {
     },
   });
 
-  useEffect(() => {
-    if (mutation.isSuccess) {
-      // Reset form fields
-      if (formRef.current) {
-        formRef.current.reset();
-      }
-      // Reset state variables
-      setOptions([]);
-      setFile(undefined);
-    }
-  }, [mutation.isSuccess]);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    // Array to hold the names of missing fields
-    const missingFields: string[] = [];
-
-    // Check for missing fields
-    if (!inputs) {
-      toast.error("Please fill in all required fields.");
-      return;
-    }
-    if (!inputs.title) {
-      missingFields.push("Title");
-    }
-    if (!inputs.desc) {
-      missingFields.push("Description");
-    }
-    if (!inputs.price) {
-      missingFields.push("Price");
-    }
-    if (!inputs?.catSlug) {
-      missingFields.push("Category");
-    }
-
-    // If there are missing fields, construct the error message dynamically
-    if (missingFields.length > 0) {
-      const errorMessage = `Please fill in the following required fields: ${missingFields.join(
-        ", "
-      )}.`;
-      toast.error(errorMessage);
-      return;
-    }
 
     try {
-      const url = await upload();
-
-      mutation.mutate({
-        img: url,
+      const validatedInputs = productSchema.parse({
         ...inputs,
         options,
+        file,
       });
+
+      if (validatedInputs) {
+        const formData = new FormData();
+
+        formData.append("title", validatedInputs.title);
+        formData.append("desc", validatedInputs.desc);
+        formData.append("price", validatedInputs.price.toString());
+        formData.append("catSlug",  validatedInputs.catSlug);
+        formData.append("img", validatedInputs.file);
+        formData.append(`options`, JSON.stringify(validatedInputs.options))
+  
+        mutation.mutate(formData);
+      }
+
     } catch (error) {
-      toast.error(error as any);
+      if (error instanceof z.ZodError) {
+        // Display validation errors
+        error.errors.forEach((err) =>toast.error(err.message));
+      } else {
+        toast.error("Unexpected error occurred");
+      }
     }
-  };
+    }
+
 
   if (status === "loading") {
     return <p>Loading...</p>;
@@ -181,18 +165,18 @@ const AddPage = () => {
       <form
         ref={formRef}
         onSubmit={handleSubmit}
-        className="w-1/2 flex flex-wrap gap-6"
+        className="w-1/2 flex flex-wrap gap-3"
       >
         <h1 className="text-4xl mb-2 text-gray-300 font-bold">
           Add New Product
         </h1>
-        <div className="w-full flex flex-col gap-2 ">
+        <div className="w-full flex flex-col gap-1">
           <label
             className="text-sm cursor-pointer flex gap-4 items-center"
             htmlFor="file"
           >
             <Image
-              src={file ? URL.createObjectURL(file as File) : "/upload.jpg"}
+              src={file ? URL.createObjectURL(file) : "/upload.jpg"}
               alt=""
               width={50}
               height={50}
@@ -206,53 +190,57 @@ const AddPage = () => {
             className="hidden"
           />
         </div>
-        <div className="w-full flex flex-col gap-2 ">
+        <div className="w-full flex flex-col gap-1 ">
           <div>
-            <label className="text-sm font-medium">Title</label>
+            <label htmlFor="title" className="text-sm font-medium">Title</label>
             <span>*</span>
           </div>
           <input
-            className="ring-1 ring-red-200 p-3 rounded-sm placeholder:text-red-200 outline-none"
+            className="ring-1 ring-red-200 p-2 rounded-sm placeholder:text-red-200 outline-none"
             type="text"
             placeholder="Bella Napoli"
             name="title"
+            id="title"
             onChange={handleChange}
           />
         </div>
-        <div className="w-full flex flex-col gap-2">
+        <div className="w-full flex flex-col gap-1">
           <div>
-            <label className="text-sm font-medium">Details</label>
+            <label htmlFor="desc" className="text-sm font-medium">Details</label>
             <span>*</span>
           </div>
           <textarea
             rows={3}
-            className="ring-1 ring-red-200 p-3 rounded-sm placeholder:text-red-200 outline-none"
+            className="ring-1 ring-red-200 p-2 rounded-sm placeholder:text-red-200 outline-none"
             placeholder="A timeless favorite with a twist, showcasing a thin crust topped with sweet tomatoes, fresh basil and creamy mozzarella."
             name="desc"
+            id="desc"
             onChange={handleChange}
           />
         </div>
-        <div className="w-full flex flex-col gap-2 ">
+        <div className="w-full flex flex-col gap-1 ">
           <div>
-            <label className="text-sm font-medium">Price</label>
+            <label htmlFor="price" className="text-sm font-medium">Price</label>
             <span>*</span>
           </div>
           <input
-            className="ring-1 ring-red-200 p-3 rounded-sm placeholder:text-red-200 outline-none"
+            className="ring-1 ring-red-200 p-2 rounded-sm placeholder:text-red-200 outline-none"
             type="number"
             placeholder="29"
             name="price"
+            id="price"
             onChange={handleChange}
           />
         </div>
-        <div className="w-full flex flex-col gap-2 ">
+        <div className="w-full flex flex-col gap-1 ">
           <div>
-            <label className="text-sm font-medium">Category</label>
+            <label htmlFor="catSlug" className="text-sm font-medium">Category</label>
             <span>*</span>
           </div>
           <select
-            className="ring-1 ring-red-200 p-3 rounded-sm placeholder:text-red-200 outline-none"
+            className="ring-1 ring-red-200 p-2 rounded-sm placeholder:text-red-200 outline-none"
             name="catSlug"
+            id="catSlug"
             onChange={handleChange}
             value={inputs?.catSlug}
           >
@@ -262,30 +250,32 @@ const AddPage = () => {
           </select>
         </div>
 
-        {/* <div className="w-full flex gap-2 ">
+         <div className="w-full flex gap-1 ">
           <div>
-            <label className="text-sm font-medium">Featured</label>
+            <label htmlFor="isFeatured" className="text-sm font-medium">Featured</label>
           </div>
           <input
             className=""
             type="checkbox"
             name="isFeatured"
+            checked={inputs?.isFeatured}
+            id="isFeatured"
             onChange={handleChange}
           />
-        </div> */}
+        </div> 
 
-        <div className="w-full flex flex-col gap-2">
-          <label className="text-sm font-medium">Options</label>
+        <div className="flex flex-col gap-1">
+          <label htmlFor="options" className="text-sm font-medium">Options</label>
           <div className="flex">
             <input
-              className="ring-1 ring-red-200 p-3 rounded-sm placeholder:text-red-200 outline-none"
+              className="ring-1 ring-red-200 p-1 rounded-sm placeholder:text-red-200 outline-none"
               type="text"
               placeholder="Title"
               name="title"
               onChange={handleChangeOptions}
             />
             <input
-              className="ring-1 ring-red-200 p-3 rounded-sm placeholder:text-red-200 outline-none"
+              className="ring-1 ring-red-200 p-1 rounded-sm placeholder:text-red-200 outline-none"
               type="number"
               placeholder="Additional Price"
               name="additionalPrice"
@@ -293,33 +283,33 @@ const AddPage = () => {
             />
             <button
               type="button"
-              className="bg-gray-500 p-2 text-white"
-              onClick={() => setOptions((prev) => [...prev, option])}
+              className="bg-gray-500 p-1 text-white"
+              onClick={handleAddOption}
             >
               Add Option
             </button>
           </div>
           <div className="flex flex-wrap gap-4 mt-2">
             {options?.map((opt) => (
-              <div
+              <button
                 key={opt.title}
                 className="p-2  rounded-md cursor-pointer bg-gray-200 text-gray-400"
                 onClick={() =>
                   setOptions((prev) =>
-                    prev.filter((item) => item.title !== opt.title)
+                    prev.filter((item) => item !== opt)
                   )
                 }
               >
                 <span>{opt.title}</span>
                 <span className="text-xs"> (+ ${opt.additionalPrice})</span>
-              </div>
+              </button>
             ))}
           </div>
         </div>
         <button
           type="submit"
           disabled={mutation.isPending}
-          className="bg-red-500 p-3 text-white w-48 rounded-md relative h-12 flex items-center justify-center"
+          className="bg-red-500 p-2 text-white w-48 rounded-md relative h-10 text-center"
         >
           {mutation.isPending ? "Submitting" : "Submit"}
         </button>
