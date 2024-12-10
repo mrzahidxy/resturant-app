4a487feaeb6d023b2cb95b4b5a49041878ed2ed8
Zahid Hasan <mrzahidxy.com>
2024-12-10 23:23:08 +0600

    config: cloudinary

---
+++ src/utils/cloudinary.ts
@@ -0,0 +1,29 @@
+import cloudinary from "./cloudinary.config";
+import { UploadApiErrorResponse, UploadApiResponse } from "cloudinary";
+
+
+type UploadResponse =
+  | { success: true; result?: UploadApiResponse }
+  | { success: false; error: UploadApiErrorResponse };
+
+export const uploadToCloudinary = (
+  fileUri: string,
+  fileName: string
+): Promise<UploadResponse> => {
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader
+      .upload(fileUri, {
+        invalidate: true,
+        resource_type: "auto",
+        filename_override: fileName,
+        folder: "protfolio-images",
+        use_filename: true,
+      })
+      .then((result) => {
+        resolve({ success: true, result });
+      })
+      .catch((error) => {
+        reject({ success: false, error });
+      });
+  });
+};
