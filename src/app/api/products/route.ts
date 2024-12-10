b39f588ca708155b4069c07c85e54514f676303f
Zahid Hasan <mrzahidxy.com>
2024-12-10 23:24:34 +0600

    modify: image upload on add product

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,3 +1,4 @@
+import { uploadToCloudinary } from "@/utils/cloudinary";
 import prisma from "@/utils/connect";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -16,17 +17,65 @@ export const GET = async (req: NextRequest) => {
 };
 
 export const POST = async (req: NextRequest) => {
-  try {
-    const body = await req.json();
 
+ 
+  try {
+    const formData = await req.formData();
+    const file = formData.get("img") as File;
+    const title = formData.get("title") as string;
+    const desc = formData.get("desc") as string;
+    const price = formData.get("price") as string;
+    const catSlug = formData.get("catSlug") as string;
+    const options = JSON.parse(formData.get("options") as string);
+
+    // Validate category exists
+    const category = await prisma.category.findUnique({
+      where: { slug: catSlug },
+    });
+
+    if (!category) {
+      return NextResponse.json(
+        { error: `Category with slug '${catSlug}' does not exist.` },
+        { status: 400 }
+      );
+    }
+
+    let img: string | undefined = undefined;
+
+    if (file) {
+      const fileBuffer = await file.arrayBuffer();
+      const base64Data = Buffer.from(fileBuffer).toString("base64");
+      const fileUri = `data:${file.type};base64,${base64Data}`;
+
+      // Upload to Cloudinary
+      const uploadRes = await uploadToCloudinary(fileUri, file.name);
+      if (!uploadRes.success) {
+        return NextResponse.json(
+          { message: "Image upload failed" },
+          { status: 500 }
+        );
+      }
+
+      img = (uploadRes as any).result.secure_url; // Get the new Cloudinary image URL
+    }
     await prisma.product.create({
-      data: { ...body },
+      data: {
+        title,
+        desc,
+        price,
+        category: {
+          connect: { slug: catSlug }, // Connect to existing category
+        },
+        img,
+        options,
+      },
     });
 
     return new NextResponse(JSON.stringify("Product is successfully created"), {
       status: 200,
     });
   } catch (error) {
+    console.log(error);
     return new NextResponse(JSON.stringify(error), { status: 500 });
   }
 };
